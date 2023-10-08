9829332868392414e2f30874ea6854dbed22d3db
chrdias <christian.dias.loegberg@gmail.com>
2023-10-08 20:19:20 +0200

    2023 route

--- flowsimulation.py
+++ flowsimulation.py
@@ -10,19 +10,28 @@ import numpy
 import matplotlib.pyplot as pyplot
 import matplotlib.patches as mpatches
 
-# 8:00, 10:15, 11:00
-groupStartTimes = {"V": 8*60, "S": 9.25*60, "OB": 10.75*60}
+# 8:00, 9:00, 10:00
+groupStartTimes = {"V": 8*60, "S": 9*60, "OB": 10*60}
 # No. of teams to start simultaneously
-tStartSimul = {"V":3, "S":3, "OB":3}
+tStartSimul = {"V":3, "S":4, "OB":4}
 tStartInterval = 15        # Time between starting teams
 tEnd = 30 * 60              # 06:00
 tActivityBuffer = 5         # Extra buffer to add to activities
 # Speeds in km/h (2021 measurements)
-meanSpeed = {"V": 3.3, "S":4, "OB": 4.3} 
-stdevSpeed = {"V": 0.4, "S":0.4, "OB":0.5}
+#meanSpeed = {"V": 3.3, "S":4, "OB": 4.3}
+#stdevSpeed = {"V": 0.4, "S":0.4, "OB":0.5}
+
+# Speeds in km/h (2022 measurements)
+meanSpeed = {"V": 3.0, "S":3.5, "OB": 4.0} 
+stdevSpeed = {"V": 0.5, "S":0.5, "OB":0.5}
+
 r = random.Random(1)
 teamTypes = ["V", "S", "OB"]
 
+# Expected
+# V: 20:30 - 03:10
+# S: 23:15 - 03:35
+# OB: 00:00 - 03:30
 class Activity(object):
 
     def __init__(self, capacity, minDuration, maxDuration, name):
@@ -254,7 +263,7 @@ def startCloseTime(act: Activity):
     allStartTimes = act.accFirstTeamStart["V"] + act.accFirstTeamStart["S"] + act.accFirstTeamStart["OB"]
     allStartTimes = [i for i in allStartTimes if i] # Remove None values
     p5StartTime = numpy.percentile(allStartTimes or [0], 5)
-    allCloseTimes = act.accLastTeamEnd["OB"]
+    allCloseTimes = act.accLastTeamEnd["V"] + act.accLastTeamEnd["S"] + act.accLastTeamEnd["OB"]
     allCloseTimes = [i for i in allCloseTimes if i] # Remove None values
     p95CloseTime = numpy.percentile(allCloseTimes or [0], 95)
     return [formatTime(p5StartTime), formatTime(p95CloseTime)]
@@ -289,95 +298,95 @@ def simulate(noOfRuns, noVTeams, noSTeams, noOBTeams):
     Post0 = Activity(8, 10, 13, "Startpost")
     Post0A = Activity(5, 10, 15, "Post 0A")
     Post0B = Activity(5, 10, 15, "Post 0B")
-    Post1 = Activity(12, 25, 40, "Post 1")
-    Post2 = Activity(5, 2, 5, "Post 2") # Død
+    Post1 = Activity(5, 10, 15, "Post 1")
+    Post2 = Activity(5, 20, 25, "Post 2")
     Post3 = Activity(5, 10, 15, "Post 3")
-    Post4 = Activity(5, 10, 15, "Post 4")
-    Post5 = Activity(5, 10, 15, "Post 5")
+    Post4 = Activity(5, 15, 20, "Post 4")
+    Post5 = Activity(5, 15, 20, "Post 5")
+    Post5A = Activity(5, 10, 15, "Post 5A")
+    Post5B = Activity(5, 10, 15, "Post 5B")
     Post6 = Activity(7, 15, 20, "Post 6")
-    Post7 = Activity(99, 2, 5, "Post 7") # Død
+    Post7 = Activity(5, 10, 15, "Post 7") # Død
     Post8 = Activity(5, 10, 15, "Post 8")
     Post9 = Activity(4, 10, 15, "Post 9") # Klatring
     Post10 = Activity(99, 60, 70, "Mad") # Opgave på madposten. Tager ikke ekstra tid
     Post11 = Activity(5, 10, 15, "Post 11")
-    Post11A = Activity(99, 2, 5, "Post 11A") # Død
     Post12 = Activity(5, 10, 15, "Post 12")
     Post13 = Activity(5, 10, 15, "Post 13")
     Post14 = Activity(5, 10, 15, "Post 14")
-    Post15 = Activity(30, 25, 40, "Post 15")
-    Post16 = Activity(5, 10, 15, "Post 16")
-    Post17 = Activity(20, 10, 60, "DFO")
+    Post15 = Activity(5, 15, 20, "Post 15")
+    Post16 = Activity(20, 10, 40, "DFO")
     PostMaal = Activity(99, None, None, "Mål")
 
-    Activities = [Post0, Post0A, Post0B, Post1, Post2, Post3, Post4, Post5, Post6, Post7, Post8,
-                Post9, Post10, Post11, Post11A, Post12, Post13, Post14, Post15, Post16, Post17, PostMaal]
+    Activities = [Post0, Post0A, Post0B, Post1, Post2, Post3, Post4, Post5, Post5A, Post5B, Post6, Post7, Post8,
+                Post9, Post10, Post11, Post12, Post13, Post14, Post15, Post16, PostMaal]
 
     """
     Link activities [act1, distance1, act2, distance2, ...]
     """
     course = {"V": [
-                Post0, 1.9,
-                Post1, 1.6,
-                Post2, 0.6,
-                Post3, 1.5,
-                Post4, 1.2,
-                Post5, 1.5,
-                Post6, 1,
-                Post7, 0.7,
-                Post8, 0.9,
-                Post9, 1.9,
-                Post10, 2.1,
-                Post11, 1.35,
-                Post12, 1.5,
-                Post13, 0,
-                Post14, 1.4,
-                Post15, 1.8,
-                Post16, 1.2,
-                Post17, 1.7,
+                Post0, 1,
+                Post1, 0.7,
+                Post2, 0.9,
+                Post3, 0.1,
+                Post4, 0.5,
+                Post5, 1.9,
+                Post6, 2.2,
+                Post7, 2.1,
+                Post8, 1.4,
+                Post9, 2.2,
+                Post10,2.5,
+                Post11,1.7,
+                Post12,1.2,
+                Post13,2.3,
+                Post14,1.7,
+                Post15,1.7,
+                Post16,1.8,
                 PostMaal],
-          "S": [Post0, 1.5,
-                Post0A, 1.8,
-                Post0B, 1.5,
-                Post1, 1.6,
-                Post2, 0.6,
-                Post3, 1.5,
-                Post4, 1.2,
-                Post5, 1.5,
-                Post6, 1,
-                Post7, 0.7,
-                Post8, 0.9,
-                Post9, 1.9,
-                Post10, 2.1,
-                Post11, 1.35,
-                Post12, 1.5,
-                Post13, 0,
-                Post14, 1.4,
-                Post15, 1.8,
-                Post16, 1.2,
-                Post17, 1.7,
+          "S": [Post0, 1.3,
+                Post0A,3,
+                Post0B,0.6,
+                Post1, 0.7,
+                Post2, 0.9,
+                Post3, 0.1,
+                Post4, 0.5,
+                Post5, 1.4,
+                Post5A,2.3,
+                Post5B,1.3,
+                Post6, 2.2,
+                Post7, 2.1,
+                Post8, 1.4,
+                Post9, 2.2,
+                Post10,2.5,
+                Post11,1.7,
+                Post12,1.2,
+                Post13,2.3,
+                Post14,1.7,
+                Post15,1.7,
+                Post16,1.8,
                 PostMaal],
           "OB": [
-                Post0, 1.5,
-                Post0A, 1.8,
-                Post0B, 1.5,
-                Post1, 1.6,
-                Post2, 0.6,
-                Post3, 1.5,
-                Post4, 1.2,
-                Post5, 1.5,
-                Post6, 1,
-                Post7, 0.7,
-                Post8, 0.9,
-                Post9, 1.9,
-                Post10, 2.1,
-                Post11, 1.8,
-                Post11A, 1.3,
-                Post12, 1.4,
-                Post13, 0,
-                Post14, 1.4,
-                Post15, 1.8,
-                Post16, 1.2,
-                Post17, 1.7,
+                Post0, 1.3,
+                Post0A,3,
+                Post0B,0.6,
+                Post1, 0.7,
+                Post2, 0.9,
+                Post3, 0.1,
+                Post4, 0.5,
+                Post5, 1.4,
+                Post5A,2.3,
+                Post5B,1.3,
+                Post6, 2.2,
+                Post7, 2.1,
+                Post8, 1.4,
+                Post9, 2.2,
+                Post10,2.5,
+                Post11,1.7,
+                Post12,1.2,
+                Post13,2.3,
+                Post14,1.7,
+                Post15,1.7,
+                Post16,1.8,
                 PostMaal]}
     """ Setup course END """
 
@@ -431,7 +440,7 @@ def simulate(noOfRuns, noVTeams, noSTeams, noOBTeams):
     title = printCourse(course["V"], "Væbnerrute",  noVTeams) + "\n"
     title += printCourse(course["S"], "Seniorrute",  noSTeams) + "\n"
     title += printCourse(course["OB"], "OB-rute", noOBTeams)
-    plotActivityStats(Activities, title)
+    # plotActivityStats(Activities, title)
 
 # Run simulation (#Runs, #VTeams, #STeams, #OBTeams)
-simulate(50, 26, 17, 10)
+simulate(50, 25, 10, 23)
